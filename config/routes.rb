7bee5a72ad368021bc173d1b6ab857041c4843b1
luisbarufi <luisbarufi@gmail.com>
2020-08-07 20:00:29 -0300

    removing files

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,11 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-
-  namespace :profiles_backoffice do
-    get 'welcome/index'
-  end
-
+  
   namespace :admins_backoffice do
     get 'welcome/index'
   end
 
-  devise_for :profiles
   devise_for :admins
   
   root 'site/welcome#index'
