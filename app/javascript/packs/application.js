c476907e1fb3544cd5bc9ee50cf12635f0eeb7f0
luisbarufi <luisbarufi@gmail.com>
2020-08-09 22:24:12 -0300

    added templates for Admins and Users

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+require("popper.js")
+require("bootstrap");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,3 +20,5 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "../src/application.css";
+import "@fortawesome/fontawesome-free/js/all";
+import './adminlte.min.js'
