cfe0dd8cdb73466246352a7b80eb4deab616d5e3
luisbarufi <luisbarufi@gmail.com>
2020-08-10 22:56:13 -0300

    added edit form for Admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
+
   namespace :site do
-    get 'welcome/index'
-  end
-  
-  namespace :admins_backoffice do
     get 'welcome/index'
   end
   
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
+  namespace :admins_backoffice do
+    get 'welcome/index'
+    resources :admins, only: [:index, :edit, :update]
+  end
   
   devise_for :users
   devise_for :admins
   
   root 'site/welcome#index'
-  
+
 end
