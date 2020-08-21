9b8bdebb6f49f6baa965c45e964d22dd9f7f8fc4
luisbarufi <luisbarufi@gmail.com>
2020-08-20 22:56:50 -0300

    refactoring the helper translate_attribute

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,10 +1,6 @@
 module AdminsBackofficeHelper
-
+  
   def translate_atribute(object = nil, method = nil)
-    if object && method
-      object.model.human_attribute_name(method)
-    else
-      "Informe os parâmetros corretamente"
-    end
+    (object && method) ? object.model.human_attribute_name(method) :  "Informe os parâmetros corretamente"
   end
 end
