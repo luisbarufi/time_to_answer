d3d08410ab2112d3bfce9f37a879ce0f611a8b80
luisbarufi <luisbarufi@gmail.com>
2020-08-27 23:31:44 -0300

    added search feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :site do
     get 'welcome/index'
+    get 'search', to: 'search#questions' # saindo um pouco do REST
   end
   
   namespace :users_backoffice do
