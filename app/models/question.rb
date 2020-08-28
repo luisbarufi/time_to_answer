5c41710a81f420129ba6053795b9b20a6ec6bc15
luisbarufi <luisbarufi@gmail.com>
2020-08-28 00:01:57 -0300

    logic transfer to the model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  def self.search(page, term)
+    Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
+  end
+
+  def self.lest_questions(page)
+    Question.includes(:answers).order('created_at desc').page(page)
+  end
 end
