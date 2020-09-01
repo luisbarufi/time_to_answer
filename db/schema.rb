b01c039e7318f57fc00513716541164ed87b41b0
luisbarufi <luisbarufi@gmail.com>
2020-09-01 00:43:02 -0300

    improving the question counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_034337) do
+ActiveRecord::Schema.define(version: 2020_09_01_031636) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_034337) do
     t.string "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
