00ea7ea8bed700fddaf223880c62b50013b7a570
luisbarufi <luisbarufi@gmail.com>
2020-09-19 23:09:22 -0300

    added statistics for admins

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  after_create :set_statistic
+
   scope :search_term_subject, ->(page, subject_id){ 
     includes(:answers, :subject).where(subject_id: subject_id).page(page) 
   }
@@ -14,4 +16,10 @@ class Question < ApplicationRecord
   scope :lest_questions, ->(page){ 
     includes(:answers, :subject).order('created_at desc').page(page) 
   }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,19 @@ class User < ApplicationRecord
   has_one :user_profile
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
 
+  after_create :set_statistic
+
   validates :first_name, presence: true, length: { minimum: 3 }, on: :update
 
   enum locale: {"pt-BR": 'pt-BR', "en": 'en'}
 
   def full_name
     [self.first_name, self.last_name].join(' ').truncate(25)
+  end
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_users])
   end
 end
